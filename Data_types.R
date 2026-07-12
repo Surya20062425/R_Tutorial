736fb8dab1da9d1a70978db87d81130f4b6a5e43
suryaprakash <b.7993974026@gmail.com>
2026-07-12 23:16:39 +0530

    Add files via upload

---
+++ Data_types.R
@@ -0,0 +1,36 @@
+# The data types are 5 types in R. They are as follows:
+# 1. Numeric    
+# 2. Character
+# 3. Logical
+# 4. Complex
+# 5. integer :in it (eg 1L, 2L, 3L) where L is used to denote integer values
+
+
+# lets Practice the data types in R
+
+#numeric data type
+num1 <- 10.5
+
+#character data type
+char1 <- "Hello"
+
+#logical data type
+log1 <- TRUE
+
+#complex data type
+comp1 <- 2 + 3i
+
+#integer data type
+int1 <- 5L
+
+# we can check the data type of a variable using the class() function
+
+class(num1)
+
+class(char1)
+
+class(log1)
+
+class(comp1)
+
+class(int1)
---
+++ Escape_characters.R
@@ -0,0 +1,11 @@
+# To insert characters that are illegal in a string, you must use an escape character.
+
+# to fix this issue, we can use the backslash (\) as an escape character. The backslash allows us to include special characters in a string without causing any errors.
+
+# For example, if we want to include a double quote (") in a string, we can use the escape character like this:
+str <- "This is a string with a \"double quote\" inside it."
+
+str
+
+cat(str)
+
